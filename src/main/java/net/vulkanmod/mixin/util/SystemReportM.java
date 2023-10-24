997643305c54cc34319158e7971a5db9e196e138
thr3343 <125277899+thr3343@users.noreply.github.com>
2023-10-24 13:24:20 +0100

    Update oshi lib to 6.4.7-snapshot (Fixes Android related crashes)

---
+++ src/main/java/net/vulkanmod/mixin/util/SystemReportM.java
@@ -0,0 +1,11 @@
+package net.vulkanmod.mixin.util;
+
+import net.minecraft.SystemReport;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+@Mixin(SystemReport.class)
+public abstract class SystemReportM {
+
+
+}
