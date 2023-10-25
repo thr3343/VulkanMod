743276362364c6b666c86ec18df989f1e443fd86
thr3343 <125277899+thr3343@users.noreply.github.com>
2023-10-25 19:06:26 +0100

    Revert "Removed unused Sampled Image Usage"
    This reverts commit dd3bd4bb8f38ff2a6a0c9d215624f127f93beb7d.

--- src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
+++ src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
@@ -524,7 +524,7 @@ public class VulkanImage {
         int format = VulkanImage.DefaultFormat;
         int formatSize;
         int mipLevels = 1;
-        int usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+        int usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
 
         boolean linearFiltering = false;
         boolean clamp = false;
