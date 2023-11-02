b25b97595e8de94cd88af0dfaf8f840a5fd74de0
thr3343 <125277899+thr3343@users.noreply.github.com>
2023-11-02 23:46:32 +0000

    Use D24 Depth if Nvidia is present

--- src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
+++ src/main/java/net/vulkanmod/vulkan/texture/VulkanImage.java
@@ -20,9 +20,10 @@ import java.util.Objects;
 import static net.vulkanmod.vulkan.Vulkan.*;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.vulkan.VK10.*;
-import static org.lwjgl.vulkan.VK12.VK_SAMPLER_REDUCTION_MODE_MIN;
 
 public class VulkanImage {
+    private static final int depthFormat = Device.findDepthFormat();
+    private static final int defDepthAspect = depthFormat == VK_FORMAT_D32_SFLOAT ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
     public static int DefaultFormat = VK_FORMAT_R8G8B8A8_UNORM;
 
     private static VkDevice device = Vulkan.getDevice();
@@ -81,7 +82,7 @@ public class VulkanImage {
         VulkanImage image = new VulkanImage(format, 1, width, height, usage, 0);
 
         image.createImage(1, width, height, format, usage);
-        image.imageView = createImageView(image.id, format, VK_IMAGE_ASPECT_DEPTH_BIT, 1);
+        image.imageView = createImageView(image.id, format, defDepthAspect, 1);
         image.createTextureSampler(blur, clamp, false);
 
         return image;
@@ -524,7 +525,7 @@ public class VulkanImage {
         barrier.subresourceRange().layerCount(1);
 
         barrier.subresourceRange()
-                .aspectMask(image.format == VK_FORMAT_D32_SFLOAT ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT);
+                .aspectMask(image.format == depthFormat ? defDepthAspect : VK_IMAGE_ASPECT_COLOR_BIT);
 
         barrier.srcAccessMask(srcAccessMask);
         barrier.dstAccessMask(dstAccessMask);
