198846f26a7ce39c370f66acc9202481adb714d1
thr3343 <125277899+thr3343@users.noreply.github.com>
2023-12-03 14:28:26 +0000

    Convert MappedBuffer to Record

--- src/main/java/net/vulkanmod/vulkan/VRenderSystem.java
+++ src/main/java/net/vulkanmod/vulkan/VRenderSystem.java
@@ -35,19 +35,19 @@ public abstract class VRenderSystem {
     public static final float clearDepth = 1.0f;
     public static FloatBuffer clearColor = MemoryUtil.memCallocFloat(4);
 
-    public static MappedBuffer modelViewMatrix = new MappedBuffer(16 * 4);
-    public static MappedBuffer projectionMatrix = new MappedBuffer(16 * 4);
-    public static MappedBuffer TextureMatrix = new MappedBuffer(16 * 4);
-    public static MappedBuffer MVP = new MappedBuffer(16 * 4);
+    public static final MappedBuffer modelViewMatrix = MappedBuffer.getMappedBuffer(16 * 4);
+    public static final MappedBuffer projectionMatrix = MappedBuffer.getMappedBuffer(16 * 4);
+    public static final MappedBuffer TextureMatrix = MappedBuffer.getMappedBuffer(16 * 4);
+    public static final MappedBuffer MVP = MappedBuffer.getMappedBuffer(16 * 4);
 
-    public static MappedBuffer ChunkOffset = new MappedBuffer(3 * 4);
-    public static MappedBuffer lightDirection0 = new MappedBuffer(3 * 4);
-    public static MappedBuffer lightDirection1 = new MappedBuffer(3 * 4);
+    public static final MappedBuffer ChunkOffset = MappedBuffer.getMappedBuffer(3 * 4);
+    public static final MappedBuffer lightDirection0 = MappedBuffer.getMappedBuffer(3 * 4);
+    public static final MappedBuffer lightDirection1 = MappedBuffer.getMappedBuffer(3 * 4);
 
-    public static MappedBuffer shaderColor = new MappedBuffer(4 * 4);
-    public static MappedBuffer shaderFogColor = new MappedBuffer(4 * 4);
+    public static final MappedBuffer shaderColor = MappedBuffer.getMappedBuffer(4 * 4);
+    public static final MappedBuffer shaderFogColor = MappedBuffer.getMappedBuffer(4 * 4);
 
-    public static MappedBuffer screenSize = new MappedBuffer(2 * 4);
+    public static final MappedBuffer screenSize = MappedBuffer.getMappedBuffer(2 * 4);
 
     public static float alphaCutout = 0.0f;
 
@@ -61,7 +61,7 @@ public abstract class VRenderSystem {
         Vulkan.initVulkan(window);
     }
 
-    public static ByteBuffer getChunkOffset() { return ChunkOffset.buffer; }
+    public static ByteBuffer getChunkOffset() { return ChunkOffset.buffer(); }
 
     public static int maxSupportedTextureSize() {
         return DeviceManager.deviceProperties.limits().maxImageDimension2D();
@@ -125,23 +125,22 @@ public abstract class VRenderSystem {
     }
 
     public static void applyModelViewMatrix(Matrix4f mat) {
-        mat.get(modelViewMatrix.buffer.asFloatBuffer());
+        mat.get(modelViewMatrix.buffer().asFloatBuffer());
         //MemoryUtil.memPutFloat(MemoryUtil.memAddress(modelViewMatrix), 1);
     }
 
     public static void applyProjectionMatrix(Matrix4f mat) {
-        mat.get(projectionMatrix.buffer.asFloatBuffer());
+        mat.get(projectionMatrix.buffer().asFloatBuffer());
     }
 
     public static void calculateMVP() {
-        org.joml.Matrix4f MV = new org.joml.Matrix4f(modelViewMatrix.buffer.asFloatBuffer());
-        org.joml.Matrix4f P = new org.joml.Matrix4f(projectionMatrix.buffer.asFloatBuffer());
-
-        P.mul(MV).get(MVP.buffer);
+        org.joml.Matrix4f MV = new org.joml.Matrix4f(modelViewMatrix.buffer().asFloatBuffer());
+        org.joml.Matrix4f P = new org.joml.Matrix4f(projectionMatrix.buffer().asFloatBuffer());
+        P.mul(MV).get(MVP.buffer());
     }
 
     public static void setTextureMatrix(Matrix4f mat) {
-        mat.get(TextureMatrix.buffer.asFloatBuffer());
+        mat.get(TextureMatrix.buffer().asFloatBuffer());
     }
 
     public static MappedBuffer getTextureMatrix() {
@@ -161,7 +160,7 @@ public abstract class VRenderSystem {
     }
 
     public static void setChunkOffset(float f1, float f2, float f3) {
-        long ptr = ChunkOffset.ptr;
+        long ptr = ChunkOffset.ptr();
         VUtil.UNSAFE.putFloat(ptr, f1);
         VUtil.UNSAFE.putFloat(ptr + 4, f2);
         VUtil.UNSAFE.putFloat(ptr + 8, f3);
