981cce00a60011efc5dba3d3fcc8ad3a3ce89de0
thr3343 <125277899+thr3343@users.noreply.github.com>
2023-12-03 14:28:26 +0000

    Convert MappedBuffer to Record

--- src/main/java/net/vulkanmod/vulkan/util/MappedBuffer.java
+++ src/main/java/net/vulkanmod/vulkan/util/MappedBuffer.java
@@ -4,22 +4,19 @@ import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
 
-public class MappedBuffer {
+public record MappedBuffer( ByteBuffer buffer, long ptr) {
 
-    public final ByteBuffer buffer;
-    public final long ptr;
 
     public static MappedBuffer createFromBuffer(ByteBuffer buffer) {
         return new MappedBuffer(buffer, MemoryUtil.memAddress0(buffer));
     }
-    MappedBuffer(ByteBuffer buffer, long ptr) {
-        this.buffer = buffer;
-        this.ptr = ptr;
+    public static MappedBuffer getMappedBuffer(ByteBuffer buffer, long ptr) {
+        return new MappedBuffer(buffer, ptr);
     }
 
-    public MappedBuffer(int size) {
-        this.buffer = MemoryUtil.memAlloc(size);
-        this.ptr = MemoryUtil.memAddress0(this.buffer);
+    public static MappedBuffer getMappedBuffer(int size) {
+        ByteBuffer buffer = MemoryUtil.memAlloc(size);
+        return new MappedBuffer(buffer, MemoryUtil.memAddress0(buffer));
     }
 
     public void putFloat(int idx, float f) {
