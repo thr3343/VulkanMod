a4a0c133f7ecf87a33a96f3064fb36398b3e20a9
thr3343 <125277899+thr3343@users.noreply.github.com>
2023-12-15 01:45:19 +0000

    Update TerrainRenderType.java

--- src/main/java/net/vulkanmod/render/vertex/TerrainRenderType.java
+++ src/main/java/net/vulkanmod/render/vertex/TerrainRenderType.java
@@ -7,33 +7,29 @@ import net.vulkanmod.vulkan.VRenderSystem;
 import java.util.EnumSet;
 
 public enum TerrainRenderType {
-    SOLID(RenderType.solid(), 0.0f, RenderType.BIG_BUFFER_SIZE),
-    CUTOUT_MIPPED(RenderType.cutoutMipped(), 0.5f, RenderType.SMALL_BUFFER_SIZE),
-    CUTOUT(RenderType.cutout(), 0.1f, RenderType.SMALL_BUFFER_SIZE),
-    TRANSLUCENT(RenderType.translucent(), 0.0f, RenderType.MEDIUM_BUFFER_SIZE),
-    TRIPWIRE(RenderType.tripwire(), 0.1f, RenderType.MEDIUM_BUFFER_SIZE);
+    SOLID(RenderType.solid(), 2097152 /*BIG_BUFFER_SIZE*/),
+    CUTOUT_MIPPED(RenderType.cutoutMipped(), 131072 /*SMALL_BUFFER_SIZE*/),
+    CUTOUT(RenderType.cutout(), 131072 /*SMALL_BUFFER_SIZE*/),
+    TRANSLUCENT(RenderType.translucent(), 262144 /*MEDIUM_BUFFER_SIZE*/),
+    TRIPWIRE(RenderType.tripwire(), 262144 /*MEDIUM_BUFFER_SIZE*/);
 
     public static final TerrainRenderType[] VALUES = TerrainRenderType.values();
 
     public static final EnumSet<TerrainRenderType> COMPACT_RENDER_TYPES = EnumSet.of(CUTOUT_MIPPED, TRANSLUCENT);
     public static final EnumSet<TerrainRenderType> ALL_RENDER_TYPES = EnumSet.allOf(TerrainRenderType.class);
 
-    final float alphaCutout;
-    public final int maxSize;  //Not sure if this should be changed to UINT16_INDEX_MAX * vertexSize
-    public final int initialSize; //Ignored W/ Per RenderTy[e AreaBuffers
 
-    TerrainRenderType(RenderType renderType, float alphaCutout, int initialSize) {
-        this.alphaCutout = alphaCutout;
+    public final int maxSize;  //Not sure if this should be changed to UINT16_INDEX_MAX * vertexSize
+    public final int initialSize; //Only used W/ Per RenderTy[e AreaBuffers
+
+    TerrainRenderType(RenderType renderType, int initialSize) {
+
         this.maxSize = renderType.bufferSize();
         this.initialSize = initialSize;
     }
 
     public static EnumSet<TerrainRenderType> getActiveLayers() {
         return !Initializer.CONFIG.fastLeavesFix ? COMPACT_RENDER_TYPES : ALL_RENDER_TYPES;
-    }
-
-    public void setCutoutUniform() {
-        VRenderSystem.alphaCutout = this.alphaCutout;
     }
 
     public static TerrainRenderType get(String renderType) {
