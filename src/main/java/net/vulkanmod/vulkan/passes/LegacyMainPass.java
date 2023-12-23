1fbb6db8ed97adf5bdeec39159754ca0da926701
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-23 09:06:32 -0300

    Update LegacyMainPass.java

--- src/main/java/net/vulkanmod/vulkan/passes/LegacyMainPass.java
+++ src/main/java/net/vulkanmod/vulkan/passes/LegacyMainPass.java
@@ -13,65 +13,64 @@ import org.lwjgl.vulkan.VkViewport;
 
 import static org.lwjgl.vulkan.VK10.*;
 
-public class LegacyMainPass implements MainPass {
-    public static LegacyMainPass PASS = new LegacyMainPass();
+public static LegacyMainPass PASS = new LegacyMainPass();
 
-    private RenderTarget mainTarget;
+private RenderTarget mainTarget;
 
-    LegacyMainPass() {
-        this.mainTarget = Minecraft.getInstance().getMainRenderTarget();
-    }
-
-    @Override
-    public void begin(VkCommandBuffer commandBuffer, MemoryStack stack) {
-    }
-
-    @Override
-    public void mainTargetBindWrite() {
-        // Caching main target for efficiency
-        mainTarget = mainTarget.orElseThrow(() -> new IllegalStateException("Main target not set"));
-        mainTarget.bindWrite(true);
-    }
-
-    @Override
-    public void mainTargetUnbindWrite() {
-        mainTarget.unbindWrite();
-    }
-
-    @Override
-    public void end(VkCommandBuffer commandBuffer) {
-        // Handling errors more gracefully
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            Renderer.getInstance().endRenderPass(commandBuffer);
-
-            mainTarget.bindRead();
-
-            SwapChain swapChain = Vulkan.getSwapChain();
-            swapChain.colorAttachmentLayout(stack, commandBuffer, Renderer.getCurrentImage());
-
-            // Refactoring for clarity
-            swapChain.beginRenderPass(commandBuffer, stack);
-            Renderer.getInstance().setBoundFramebuffer(swapChain);
-
-            VkViewport.Buffer pViewport = swapChain.viewport(stack);
-            vkCmdSetViewport(commandBuffer, 0, pViewport);
-
-            VkRect2D.Buffer pScissor = swapChain.scissor(stack);
-            vkCmdSetScissor(commandBuffer, 0, pScissor);
-
-            // Disable blend for legacy pass
-            VRenderSystem.disableBlend();
-
-            mainTarget.blitToScreen(swapChain.getWidth(), swapChain.getHeight());
-        } catch (RuntimeException e) {
-            // More specific exception handling
-            System.err.println("Failed to record main pass: " + e.getMessage());
-        }
-
-        // Handling end command buffer errors more gracefully
-        int result = vkEndCommandBuffer(commandBuffer);
-        if (result != VK_SUCCESS) {
-            System.err.println("Failed to end command buffer: " + result);
-        }
-    }
+LegacyMainPass() {
+    this.mainTarget = Minecraft.getInstance().getMainRenderTarget();
 }
+
+@Override
+public void begin(VkCommandBuffer commandBuffer, MemoryStack stack) {
+    // No-op
+}
+
+@Override
+public void mainTargetBindWrite() {
+    RenderTarget mainTarget = Minecraft.getInstance().getMainRenderTarget();
+    mainTarget.bindWrite(true);
+}
+
+@Override
+public void mainTargetUnbindWrite() {
+    RenderTarget mainTarget = Minecraft.getInstance().getMainRenderTarget();
+    mainTarget.unbindWrite();
+}
+
+@Override
+public void end(VkCommandBuffer commandBuffer) {
+    try (MemoryStack stack = MemoryStack.stackPush()) {
+        // Remove redundant `mainTarget.bindRead()` call
+
+        SwapChain swapChain = Vulkan.getSwapChain();
+        swapChain.colorAttachmentLayout(stack, commandBuffer, Renderer.getCurrentImage());
+
+        // Combine render passes
+        swapChain.beginRenderPass(commandBuffer, stack);
+        Renderer.getInstance().setBoundFramebuffer(swapChain);
+
+        // Set viewport and scissor once per frame
+        VkViewport.Buffer pViewport = swapChain.viewport(stack);
+        vkCmdSetViewport(commandBuffer, 0, pViewport);
+        VkRect2D.Buffer pScissor = swapChain.scissor(stack);
+        vkCmdSetScissor(commandBuffer, 0, pScissor);
+
+        // Re-enable blending if necessary
+        if (VRenderSystem.isBlendingDisabled()) {
+            VRenderSystem.enableBlending();
+        }
+
+        Minecraft.getInstance().getMainRenderTarget().blitToScreen(swapChain.getWidth(), swapChain.getHeight());
+    }
+
+    Renderer.getInstance().endRenderPass(commandBuffer);
+
+    int result = vkEndCommandBuffer(commandBuffer);
+    if (result != VK_SUCCESS) {
+        throw new RuntimeException("Failed to record command buffer:" + result);
+    }
+
+}
+
+  
