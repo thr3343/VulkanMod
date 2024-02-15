8864d73574adf53fe450217ee70d81ea0ee69c8f
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-02-15 13:32:43 +0000

    Adjust UBO+Aux VertexBuffers Sizes + Fix Glass Panes on Fast Graphics

---
+++ src/main/java/net/vulkanmod/mixin/render/ItemBlockRenderTypesM.java
@@ -0,0 +1,22 @@
+package net.vulkanmod.mixin.render;
+
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
+
+@Mixin(ItemBlockRenderTypes.class)
+public class ItemBlockRenderTypesM {
+
+    @Shadow @Final private static Map<Block, RenderType> TYPE_BY_BLOCK;
+
+    static {
+        TYPE_BY_BLOCK.remove(Blocks.GLASS_PANE);
+        TYPE_BY_BLOCK.put(Blocks.GLASS_PANE, RenderType.translucent());
+    }
+}
