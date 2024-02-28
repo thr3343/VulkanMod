8b0bdd339021310dcafcac5cd0591d6dbfb14883
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-02-28 15:13:16 +0000

    Remove VSync Frame limit

--- src/main/java/net/vulkanmod/vulkan/framebuffer/SwapChain.java
+++ src/main/java/net/vulkanmod/vulkan/framebuffer/SwapChain.java
@@ -450,6 +450,6 @@ public class SwapChain extends Framebuffer {
     public RenderPass getRenderPass() {
         return renderPass;
     }
-    public int getFramesNum() { return this.vsync? 1 : Initializer.CONFIG.frameQueueSize; }
+    public int getFramesNum() { return Initializer.CONFIG.frameQueueSize; }
     public int getImagesNum() { return this.swapChainImages.size(); }
 }
