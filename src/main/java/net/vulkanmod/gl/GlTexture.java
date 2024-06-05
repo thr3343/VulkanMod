02e283f586f2bb40ec4f6837f613f50bac7988d5
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-06-05 22:06:51 +0100

    Update GlTexture.java

--- src/main/java/net/vulkanmod/gl/GlTexture.java
+++ src/main/java/net/vulkanmod/gl/GlTexture.java
@@ -1,6 +1,7 @@
 package net.vulkanmod.gl;
 
 import it.unimi.dsi.fastutil.ints.Int2ReferenceOpenHashMap;
+import net.vulkanmod.vulkan.Renderer;
 import net.vulkanmod.vulkan.memory.MemoryManager;
 import net.vulkanmod.vulkan.texture.SamplerManager;
 import net.vulkanmod.vulkan.texture.ImageUtil;
@@ -53,7 +54,11 @@ public class GlTexture {
         GlTexture glTexture = map.remove(i);
         VulkanImage image = glTexture != null ? glTexture.vulkanImage : null;
         if(image != null)
+        {
             MemoryManager.getInstance().addToFreeable(image);
+        }
+//        removeImageResource(i);
+        Renderer.getDescriptorSetArray().removeImage(i);
     }
 
     public static GlTexture getTexture(int id) {
