c9893b74c6074df68d4b49f545207376122a258d
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-09-16 14:19:33 +0100

    Use Non-Inverted Viewport (Inverted Y NDC)

--- src/main/java/net/vulkanmod/mixin/matrix/Matrix4fM.java
+++ src/main/java/net/vulkanmod/mixin/matrix/Matrix4fM.java
@@ -1,9 +1,13 @@
 package net.vulkanmod.mixin.matrix;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Matrix4f.class)
 public abstract class Matrix4fM {
@@ -17,7 +21,8 @@ public abstract class Matrix4fM {
      */
     @Overwrite(remap = false)
     public Matrix4f setOrtho(float left, float right, float bottom, float top, float zNear, float zFar) {
-        return new Matrix4f().setOrtho(left, right, bottom, top, zNear, zFar, true);
+        //Flip ortho Top and Bottom for Vulkan NDC
+        return new Matrix4f().setOrtho(left, right, top, bottom, zNear, zFar, true);
     }
 
     /**
@@ -26,7 +31,8 @@ public abstract class Matrix4fM {
      */
     @Overwrite(remap = false)
     public Matrix4f ortho(float left, float right, float bottom, float top, float zNear, float zFar) {
-        return this.ortho(left, right, bottom, top, zNear, zFar, true);
+        //Flip ortho Top and Bottom for Vulkan NDC
+        return this.ortho(left, right, top, bottom, zNear, zFar, true);
     }
 
     /**
@@ -46,4 +52,15 @@ public abstract class Matrix4fM {
     public Matrix4f setPerspective(float fovy, float aspect, float zNear, float zFar) {
         return new Matrix4f().setPerspective(fovy, aspect, zNear, zFar, true);
     }
+
+    @WrapOperation(method = "setPerspective(FFFFZ)Lorg/joml/Matrix4f;",
+            at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;_m11(F)Lorg/joml/Matrix4f;"), remap = false)
+    private Matrix4f flipPerspectiveY(Matrix4f instance, float m11, Operation<Matrix4f> original)
+    {
+        //invertNDCY or flipPerspectiveY
+
+        //Invert the y component of the Perspective Matrix
+        return original.call(instance, m11*-1.0f);
+    }
+
 }
