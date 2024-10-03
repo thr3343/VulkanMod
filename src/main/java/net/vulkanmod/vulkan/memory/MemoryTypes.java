2377b94bff4e29da36fcfbfd64150cf20b1c9af3
thr3343 <125277899+thr3343@users.noreply.github.com>
2024-10-03 14:00:37 +0100

    Update MemoryTypes.java

--- src/main/java/net/vulkanmod/vulkan/memory/MemoryTypes.java
+++ src/main/java/net/vulkanmod/vulkan/memory/MemoryTypes.java
@@ -25,8 +25,8 @@ public class MemoryTypes {
 
             }
 
-            if (memoryType.propertyFlags() == (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | VK_MEMORY_PROPERTY_HOST_CACHED_BIT)) {
-                HOST_MEM = new HostLocalCachedMemory(memoryType, heap, MemoryTypeIndex);
+            if (memoryType.propertyFlags() == (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
+                HOST_MEM = new HostLocalUncachedMemory(memoryType, heap, MemoryTypeIndex);
             }
         }
 
@@ -117,9 +117,9 @@ public class MemoryTypes {
         }
     }
 
-    static class HostLocalCachedMemory extends MappableMemory {
+    static class HostLocalUncachedMemory extends MappableMemory {
 
-        HostLocalCachedMemory(VkMemoryType vkMemoryType, VkMemoryHeap vkMemoryHeap, int memoryTypeIndex) {
+        HostLocalUncachedMemory(VkMemoryType vkMemoryType, VkMemoryHeap vkMemoryHeap, int memoryTypeIndex) {
             super(Type.HOST_LOCAL, vkMemoryType, vkMemoryHeap, memoryTypeIndex);
         }
 
@@ -128,7 +128,7 @@ public class MemoryTypes {
 
             MemoryManager.getInstance().createBuffer(buffer, size,
                     VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT | buffer.usage,
-                    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | VK_MEMORY_PROPERTY_HOST_CACHED_BIT);
+                    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
         }
 
         void copyToBuffer(Buffer buffer, long dstOffset, long bufferSize, ByteBuffer byteBuffer) {
