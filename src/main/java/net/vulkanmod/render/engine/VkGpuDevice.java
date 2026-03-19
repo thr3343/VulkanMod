0e2447fa92f291d8e7d77d640cfdd158343883fa
xCollateral <103696619+xCollateral@users.noreply.github.com>
2026-03-19 18:12:08 +0100

    Add device name to debug system specs

--- src/main/java/net/vulkanmod/render/engine/VkGpuDevice.java
+++ src/main/java/net/vulkanmod/render/engine/VkGpuDevice.java
@@ -182,7 +182,7 @@ public class VkGpuDevice implements GpuDevice {
 
     @Override
     public String getRenderer() {
-        return "VulkanMod %s".formatted(Initializer.getVersion()) ;
+        return DeviceManager.device.deviceName;
     }
 
     @Override
