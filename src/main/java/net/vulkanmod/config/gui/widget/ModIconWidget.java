f768a758a237a922e7efffb5f21c827fb82c453e
amiralimollaei <me.amiralimollaei@gmail.com>
2026-06-09 16:01:45 +0330

    fix bugs in `ModIconWidget`

--- src/main/java/net/vulkanmod/config/gui/widget/ModIconWidget.java
+++ src/main/java/net/vulkanmod/config/gui/widget/ModIconWidget.java
@@ -1,7 +1,9 @@
 package net.vulkanmod.config.gui.widget;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FormattedText;
 import net.minecraft.resources.Identifier;
@@ -22,6 +24,10 @@ public class ModIconWidget extends VAbstractWidget {
         this.height = height;
     }
 
+    @Override
+    public void playDownSound(SoundManager soundManager) {
+    }
+
     public void render(double mX, double mY) {
         int backgroundColor = ColorUtil.ARGB.multiplyAlpha(VGuiConstants.COLOR_BLACK, 0.6f);
         int width = this.width;
@@ -29,12 +35,15 @@ public class ModIconWidget extends VAbstractWidget {
         GuiRenderer.fill(this.x, this.y, this.x + width, this.y + height, backgroundColor);
 
 
-        int size = this.height - 4;
+        int iconSize = this.height - 4;
         int iconX = this.x + 4;
-        int iconY = this.y + (height - size) / 2;
-        GuiRenderer.guiGraphics.blit(RenderPipelines.GUI_TEXTURED, icon, iconX, iconY, 0f, 0f, size, size, size, size);
+        int iconY = this.y + (height - iconSize) / 2;
+        GuiRenderer.guiGraphics.blit(RenderPipelines.GUI_TEXTURED, icon, iconX, iconY, 0f, 0f, iconSize, iconSize, iconSize, iconSize);
+        Font textRenderer = Minecraft.getInstance().font;
 
-        size = this.height;
-        GuiRenderer.drawString(Minecraft.getInstance().font, (Component) this.name, this.x + 6 + size, this.y + this.height / 2 - 4, 0xffffffff);
+        int size = this.height;
+        int maxWidth = this.width - iconSize - 8;
+
+        GuiRenderer.drawScrollingString(textRenderer, (Component) this.name, this.x + size + maxWidth / 2 + 3, this.y + this.height / 2 - 4, maxWidth - 4, 0xffffffff);
     }
 }
